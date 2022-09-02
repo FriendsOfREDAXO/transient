0944c008ba47fcaaf6e87a43096a77d676608c89
eaCe <webmaster@slaps.de>
2022-09-02 09:03:09 +0200

    wip: try something new... :|

--- .tools/bootstrap.php
+++ .tools/bootstrap.php
@@ -2,12 +2,12 @@
 
 unset($REX);
 $REX['REDAXO'] = true;
-$REX['HTDOCS_PATH'] = '../../../../../';
+$REX['HTDOCS_PATH'] = '../../../../';
 $REX['BACKEND_FOLDER'] = 'redaxo';
 $REX['LOAD_PAGE'] = false;
 
-var_dump(realpath('../../../../../') . PHP_EOL);
-var_dump(realpath('../../../core/boot.php') . PHP_EOL);
+var_dump(realpath('../../../../'));
+var_dump(realpath('../../../core/boot.php'));
 
 require '../../../core/boot.php';
 require '../../../core/packages.php';
