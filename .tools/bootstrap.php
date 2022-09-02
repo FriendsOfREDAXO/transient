f1866c7b1173ea54af0a078fecf18161f5375620
eaCe <webmaster@slaps.de>
2022-09-02 08:57:24 +0200

    wip: try something new... :|

--- .tools/bootstrap.php
+++ .tools/bootstrap.php
@@ -1,15 +1,13 @@
 <?php
 
-chdir(dirname(__DIR__).'/redaxo_cms/redaxo');
-
 unset($REX);
 $REX['REDAXO'] = true;
-$REX['HTDOCS_PATH'] = '../';
+$REX['HTDOCS_PATH'] = '../../../../../';
 $REX['BACKEND_FOLDER'] = 'redaxo';
 $REX['LOAD_PAGE'] = false;
 
-require dirname(__DIR__).'/redaxo_cms/redaxo/src/core/boot.php';
-require dirname(__DIR__).'/redaxo_cms/redaxo/src/core/packages.php';
+require '../../../core/boot.php';
+require '../../../core/packages.php';
 
 // use original error handlers of the tools
 rex_error_handler::unregister();
