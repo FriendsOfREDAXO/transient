3e6ac0d8ea7dae0cdb1b68d07db068d67fa2bd12
eaCe <webmaster@slaps.de>
2022-09-02 09:00:55 +0200

    wip: try something new... :|

--- .tools/bootstrap.php
+++ .tools/bootstrap.php
@@ -6,6 +6,9 @@ $REX['HTDOCS_PATH'] = '../../../../../';
 $REX['BACKEND_FOLDER'] = 'redaxo';
 $REX['LOAD_PAGE'] = false;
 
+var_dump(realpath('../../../../../') . PHP_EOL);
+var_dump(realpath('../../../core/boot.php') . PHP_EOL);
+
 require '../../../core/boot.php';
 require '../../../core/packages.php';
 
